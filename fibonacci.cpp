bde7b8f4f98ff605b4bbc968b8f31e5590c8a8b4
Zhenia <j.berenchan@gmail.com>
2022-07-23 19:20:51 +0200

    add fibonacci alg

---
+++ fibonacci.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int find_fibonacci(int x) {
+    if (x < 2)
+        return x;
+
+    return find_fibonacci(x - 1) + find_fibonacci(x - 2);
+}
+
+int fibonacci(int x) {
+    cout << "Fibonacci for (" << x << ") = " << find_fibonacci(x) << endl;
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 void recsearch(int x);
 void factorial(int x);
+void fibonacci(int x);
+
 int main() {
     // your func there :)
 }
